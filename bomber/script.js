b7f55c4287ab3c43f4fa0ed3d7e1a2f7d2eae4d5
Yuan QU <64603772+yq86@users.noreply.github.com>
2020-07-01 16:44:49 +0200

    kill doesn't work yet
    size calculated but not working yet

--- bomber/script.js
+++ bomber/script.js
@@ -1,16 +1,18 @@
 let gameZone = document.querySelector(".game-zone");
 let player = document.querySelector(".player");
+let bomb = document.querySelector("#bomb");
 let enemy1 = document.querySelector(".enemy1");
 let enemy2 = document.querySelector(".enemy2");
 let enemy3 = document.querySelector(".enemy3");
 let enemy4 = document.querySelector(".enemy4");
 let enemy5 = document.querySelector(".enemy5");
-let bomb = document.querySelector("#bomb");
+
 
 let gWidth = parseInt(window.getComputedStyle(gameZone).getPropertyValue("width"));
 let gHeight = parseInt(window.getComputedStyle(gameZone).getPropertyValue("height"));
 let pTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
 let pLeft = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+
 
 let e1Top = parseInt(window.getComputedStyle(enemy1).top);
 let e1Left = parseInt(window.getComputedStyle(enemy1).left);
@@ -22,6 +24,7 @@ let e4Top = parseInt(window.getComputedStyle(enemy4).top);
 let e4Left = parseInt(window.getComputedStyle(enemy4).left);
 let e5Top = parseInt(window.getComputedStyle(enemy5).top);
 let e5Left = parseInt(window.getComputedStyle(enemy5).left);
+
 
 // player move
 document.onkeydown = function(e){
@@ -45,28 +48,13 @@ document.onkeydown = function(e){
             pLeft += 20;
             player.style.left = pLeft + "px";
         }
-    } else if (e.keyCode == '32') {  
-// bomb appears        
-        bomb.classList.add("bang");
-        bomb.style.left = pLeft+"px";
-        bomb.style.top = pTop+"px";
-// bomb disappears and size doubling
-        window.setTimeout(function(){
-            bomb.style.width = 80+"px";
-            bomb.style.height= 80+ "px"; 
-        }, 2000);
-        window.setTimeout(function(){
-            bomb.classList.remove("bang");
-            bomb.style.width = 40+"px";
-            bomb.style.height= 40+ "px";
-        }, 3000);
-    }
+    } 
+
 };   
 
 
-
-
 // enemy move
+
 window.setInterval(function(){
     let i = Math.floor(Math.random() * 4);
         if (i==0) {
@@ -127,7 +115,61 @@ window.setInterval(function(){
 
 
 
-/*  thoughts of player ph and enemy being killed
+
+
+document.addEventListener("keydown", function(e){
+
+    if (e.keyCode == '32') {  
+        
+// bomb appears        
+        bomb.classList.add("bang");
+        var bLeft = bomb.style.left = pLeft+"px";
+        var bTop = bomb.style.top = pTop+"px";
+
+// bomb size doubling in 2 seconds
+        window.setTimeout(function(){
+            bomb.style.width = 80+"px";
+            bomb.style.height= 80+ "px"; 
+        }, 2000);
+        window.setTimeout(function(){
+// suppose to kill enemies here
+        if (bLeft+40 > e1Left > bLeft-40 && bTop+40 > e1Top > bTop-40) {
+            enemy1.classList.remove("enemy1");
+            } else if (bLeft+40 > e2Left > bLeft-40 && bTop+40 > e2Top > bTop-40){
+                enemy2.classList.remove("enemy2");
+            } else if (bLeft+40 > e3Left > bLeft-40 && bTop+40 > e3Top > bTop-40){
+                enemy3.classList.remove("enemy3");
+            } else if (bLeft+40 > e4Left > bLeft-40 && bTop+40 > e4Top > bTop-40){
+                enemy4.classList.remove("enemy4");
+            } else if (bLeft+40 > e5Left > bLeft-40 && bTop+40 > e5Top > bTop-40){
+                enemy5.classList.remove("enemy5");
+            } 
+             console.log("bomb: "+bTop);
+             console.log("bomb: "+bLeft);
+             console.log("enemy1: "+e1Top);
+             console.log("enemy1: "+e1Left);
+             
+// bomb disappear in 3 seconds and size back to normal
+            bomb.classList.remove("bang");
+// if I dont do the following, the next time I throw a bomb the size stays double
+            bomb.style.width = 40+"px";
+            bomb.style.height= 40+ "px";
+            
+        }, 3000);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+/*  thoughts of player ph-- and enemy being killed
 
 if (eLeft+40 > pLeft > eLeft-40 && eTop+40 > pTop > eTop-40) {
     player ph--;
@@ -136,4 +178,50 @@ if (eLeft+40 > pLeft > eLeft-40 && eTop+40 > pTop > eTop-40) {
 if (bLeft+40 > eLeft > bLeft-40 && bTop+40 > eTop > bTop-40) {
     remove enemy class;
 }
+
+-----------------------------------
+
+------------------------------------
+// to kill enemies
+IF bomb size doubled when it disappears, 
+
+THEN
+if (bLeft+80 > eLeft > bLeft-40 && bTop+80 > eTop > bTop-40) {
+    remove enemy class;
+}
+
+THEN
+if (bLeft+80 > e1Left > bLeft-40 && bTop+80 > e1Top > bTop-40) {
+    enemy1.classList.remove("enemy1");
+} else if (bLeft+80 > e2Left > bLeft-40 && bTop+80 > e2Top > bTop-40) {
+    enemy2.classList.remove("enemy2");
+} else if (bLeft+80 > e3Left > bLeft-40 && bTop+80 > e3Top > bTop-40) {
+    enemy3.classList.remove("enemy3");
+}  else if (bLeft+80 > e4Left > bLeft-40 && bTop+80 > e4Top > bTop-40) {
+    enemy4.classList.remove("enemy4");
+} else if (bLeft+80 > e5Left > bLeft-40 && bTop+80 > e5Top > bTop-40) {
+    enemy5.classList.remove("enemy5");
+}           
+----------------------------------------
+// to kill enemies
+IF bomb size didnt double when it disappears
+
+THEN
+if (bLeft+40 > eLeft > bLeft-40 && bTop+40 > eTop > bTop-40) {
+    enemy.classList.remove("enemy");
+}
+
+THEN
+if (bLeft+40 > e1Left > bLeft-40 && bTop+40 > e1Top > bTop-40) {
+    enemy1.classList.remove("enemy1");
+    } else if (bLeft+40 > e2Left > bLeft-40 && bTop+40 > e2Top > bTop-40){
+        enemy2.classList.remove("enemy2");
+    } else if (bLeft+40 > e3Left > bLeft-40 && bTop+40 > e3Top > bTop-40){
+        enemy3.classList.remove("enemy3");
+    } else if (bLeft+40 > e4Left > bLeft-40 && bTop+40 > e4Top > bTop-40){
+        enemy4.classList.remove("enemy4");
+    } else if (bLeft+40 > e5Left > bLeft-40 && bTop+40 > e5Top > bTop-40){
+        enemy5.classList.remove("enemy5");
+    }    
+-------------------------------------             
 */
